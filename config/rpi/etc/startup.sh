e8ead92950e0e9166b2b01dd14349c62dcb6efd4
Jens Steinhauser <jens.steinhauser@gmail.com>
2012-10-01 10:33:06 +0200

    [rpi] Wait for the ethernet chip to be ready.
    Signed-off-by: Jens Steinhauser <jens.steinhauser@gmail.com>

--- config/rpi/etc/startup.sh
+++ config/rpi/etc/startup.sh
@@ -10,3 +10,8 @@ mount -a
 
 # Set log level to LOG_INFO (debug messages are not logged)
 syslogd -C -l 7
+
+# Because the ethernet chip is connected via USB, it is not always
+# ready right after bootup. Wait a few seconds so that it is ready
+# when the network package wants to bring it up.
+sleep 2
